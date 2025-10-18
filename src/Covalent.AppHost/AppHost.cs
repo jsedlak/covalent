a0902c9a45d9bb13c390dab6ff59731985dc180a
= <john.sedlak@outlook.com>
2025-10-18 16:38:28 -0400

    Moves to Aspire Foundry integration

--- src/Covalent.AppHost/AppHost.cs
+++ src/Covalent.AppHost/AppHost.cs
@@ -1,22 +1,19 @@
+using Aspire.Hosting.Azure;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var foundry = builder.AddAzureFoundry("namedAzureFoundry");
-var ollama = builder.AddOllama("ollama")
-    .WithOpenWebUI()
-    .WithDataVolume();
-var model = ollama.AddModel("gemma3:1b");
+var foundry = builder.AddAzureAIFoundry("foundry");
+var chat = foundry.AddDeployment("chat", AIFoundryModel.OpenAI.Gpt5Nano);
 
 var silo = builder.AddProject<Projects.Covalent_Silo>("covalent-silo")
+    .WithReference(chat)
     .WithReference(foundry)
-    .WithReference(ollama)
     .WithExternalHttpEndpoints();
 
 silo.WithUrl("/api/providers", "Providers");
 
 var app = builder.AddNpmApp("covalent-app", "../../covalent-app", "dev")
     .WithReference(silo)
-    .WithReference(ollama)
-    .WithReference(model)
     .WaitFor(silo)
     .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
     .WithHttpEndpoint(env: "PORT")
