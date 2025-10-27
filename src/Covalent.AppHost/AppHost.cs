2933c6b110ea12c17663a8ccb84105a71400fd2a
= <john.sedlak@outlook.com>
2025-10-26 21:16:57 -0400

    Adds hitch plugin loading

--- src/Covalent.AppHost/AppHost.cs
+++ src/Covalent.AppHost/AppHost.cs
@@ -1,13 +1,21 @@
 using Aspire.Hosting.Azure;
+using Hitch.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+// configure our plugins!
+var hitch = builder.AddHitch(builder => builder.WithFilePattern("Covalent.*.Plugin.dll"));
 
 var foundry = builder.AddAzureAIFoundry("foundry");
 var chat = foundry.AddDeployment("chat", AIFoundryModel.OpenAI.Gpt5Nano);
 
+var temporal = builder.AddTemporalServerContainer("temporal");
+
 var silo = builder.AddProject<Projects.Covalent_Silo>("covalent-silo")
+    .WithReference(hitch)
     .WithReference(chat)
     .WithReference(foundry)
+    .WithReference(temporal)
     .WithExternalHttpEndpoints();
 
 silo.WithUrl("/api/providers", "Providers");
